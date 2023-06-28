a9a74662e452039d1666f503b6f341dd1ab40179
Luca Alfano <luca.alfano@t-online.de>
2023-06-28 14:31:33 +0200

    first commit

---
+++ run.sh
@@ -0,0 +1,8 @@
+xhost +
+sudo docker run -it \
+    --device=/dev/dri \
+    --group-add video \
+    --volume=/tmp/.X11-unix:/tmp/.X11-unix \
+    --env="DISPLAY=$DISPLAY" \
+    --network=host \
+    rviz
